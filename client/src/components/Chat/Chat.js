9eb2958cee717322c69ad5b27e238c6014b8bf2e
ndm0501 <nadeem.endeavour@gmail.com>
2020-01-28 17:11:07 +0530

    added socket

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 import "./Chat.css";
-const Chat = () => {
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const URL = "localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io(URL);
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+  }, [URL, location.search]);
   return <h1>Chat</h1>;
 };
 export default Chat;
